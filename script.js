a5c5199c89e219079f1d56d3a1acfaf5ee86c5a0
eswarprasadk <eswarprasadk@turito.com>
2023-07-02 19:38:08 +0530

    Twitter url logic added

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 let apiQuotes = [];
+let randomItem = {};
+const loader = document.getElementById("loader");
 
 // Get quotes API function
 async function getQuotes() {
+  toggleLoader(true);
   const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
   try {
     apiQuotes = await (await fetch(apiUrl)).json();
@@ -10,13 +13,40 @@ async function getQuotes() {
     // Catch error
     alert(error);
   }
+  setTimeout(() => {
+    toggleLoader(false);
+  }, 1000);
 }
 
 function getRandomQuote() {
+  toggleLoader(true);
   randomItem = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-  document.getElementById("quote").innerHTML = randomItem.text;
-  document.getElementById("author").innerHTML = randomItem.author;
+  const quoteElem = document.getElementById("quote");
+  quoteElem.textContent = randomItem.text;
+  document.getElementById("author").textContent =
+    randomItem.author || "Unknown";
+  // Check if quote length is long , then append long text css class
+  randomItem.text.length > 120
+    ? quoteElem.classList.add("long-quote")
+    : quoteElem.classList.remove("long-quote");
+  setTimeout(() => {
+    toggleLoader(false);
+  }, 1000);
+}
+
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${
+    randomItem.text
+  } - ${randomItem.author || "Unknown"}`;
+  window.open(twitterUrl, "_blank");
+}
+
+function toggleLoader(state) {
+  document.getElementById("quote-container").hidden = state;
+  loader.hidden = !state;
 }
 
 // On Load
+document.getElementById("new").addEventListener("click", getRandomQuote);
+document.getElementById("twitter").addEventListener("click", tweetQuote);
 getQuotes();
